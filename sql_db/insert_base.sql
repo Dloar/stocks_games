0a32685ae7d9a10a3e7eea2e4440fc35a9c0fae7
dloar <ondrej_kral@hotmail.com>
2020-04-08 21:15:55 +0200

    dev: adding all stacks.

--- sql_db/insert_base.sql
+++ sql_db/insert_base.sql
@@ -1,7 +1,20 @@
 delete from stocks_list
-INSERT INTO stocks_list (stock_name, stock_symbol)
-VALUES('Merck', 'MRK'),
-('Amazon', 'AMZN'), ('BMW', 'BMW.DE'), ('Boeing', 'BA'), ('NVIDIA', 'NVDA'), ('Veolia', 'VIE.PA'),
- ('Lotos', ''), ('Avast', 'AVST.PR'), ('Qualcomm', 'QCOM'), ('Microsoft', 'MSFT'), ('Intel', 'INTC'),
- ('KB', 'KOMB.PR'), ('Total SA', 'TOT'), ('Alibaba', 'BABA'), ('Amgen', 'AMGN'), ('Blizzard', 'ATVI'),
- ('Moneta', 'MONET.PR')
+INSERT INTO stocks_list
+    (stock_name, stock_symbol)
+VALUES
+    ('Merck', 'MRK'),
+    ('Amazon', 'AMZN'),
+    ('BMW', 'BMW.DE'),
+    ('Boeing', 'BA'),
+    ('NVIDIA', 'NVDA'),
+    ('Veolia', 'VIE.PA'),
+    ('Avast', 'AVST.PR'),
+    ('Qualcomm', 'QCOM'),
+    ('Microsoft', 'MSFT'),
+    ('Intel', 'INTC'),
+    ('KB', 'KOMB.PR'),
+    ('Total SA', 'TOT'),
+    ('Alibaba', 'BABA'),
+    ('Amgen', 'AMGN'),
+    ('Blizzard', 'ATVI'),
+    ('Moneta', 'MONET.PR')
