cb21a0dfb1652816d2b9cf7f85e66df3b8926fa5
dloar <ondrej_kral@hotmail.com>
2020-05-05 20:25:13 +0100

    dev: preparation to send email from python.

---
+++ tech_dev/print.py
@@ -0,0 +1,4 @@
+from datetime import datetime
+
+cur_time = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
+print("Currently is " + cur_time)
---
+++ tech_dev/update_daily
@@ -0,0 +1,3 @@
+# This script send daily mail with update.
+echo "This is working."
+
