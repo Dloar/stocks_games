709be247b85d2529b1adbaf55d2648ee16ad326f
dloar <ondrej_kral@hotmail.com>
2020-05-20 21:55:39 +0200

    dev: update insert_base names.

--- python_dev/daily_update.py
+++ python_dev/daily_update.py
@@ -7,4 +7,5 @@ stocks_data = loadData()
 invested_amount = stocks_data.stocks_exposures['exposure'].sum()
 
 stocks_volume_df = stocks_data.stocks_volume.loc[stocks_data.stocks_volume['curr_volume'] > 0]
-
+list_of_owned_stocks = pd.merge(stocks_volume_df, stocks_data.stocks_list[['stock_name', 'stock_symbol']],
+                                how='left', on='stock_name')
